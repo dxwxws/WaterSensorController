1e0e09b0ea3180a4b467c6ead5928bcf126fef51
dxwxws <zhang7293@126.com>
2026-04-03 12:24:40 +0800

    注释掉了所有的有关调试的代码，以提高速度。

--- src/modbus.c
+++ src/modbus.c
@@ -7,7 +7,7 @@
 
 #define TRUE  1
 #define FALSE 0
-#define MODBUS_DEBUG 0
+// #define MODBUS_DEBUG 0
 
 // 环形缓冲区（串口“DMA 接收缓存”）
 __xdata uint8_t modbus_rx_buf[MODBUS_RX_BUF_SIZE];
@@ -163,9 +163,9 @@ void Modbus_Task(void)
 
     if (crcCalc != crcRecv)
     {
-    #if MODBUS_DEBUG
-        UART_SendBytes((uint8_t*)"ERR:CRC\r\n", 9);
-    #endif
+    // #if MODBUS_DEBUG
+    //     UART_SendBytes((uint8_t*)"ERR:CRC\r\n", 9);
+    // #endif
         modbus_frame_len = 0;
         return; // CRC 错误
     }
@@ -174,9 +174,9 @@ void Modbus_Task(void)
     // 地址判断
     if (modbus_frame_buf[0] != g_slave_id)
     {
-    #if MODBUS_DEBUG
-        UART_SendBytes((uint8_t*)"IGN:ADDR\r\n", 10);
-    #endif
+    // #if MODBUS_DEBUG
+    //     UART_SendBytes((uint8_t*)"IGN:ADDR\r\n", 10);
+    // #endif
         modbus_frame_len = 0;
         return; // 不是发给我的
     }
@@ -195,9 +195,9 @@ void Modbus_Task(void)
                 break;
 
             default:
-            #if MODBUS_DEBUG
-                UART_SendBytes((uint8_t*)"ERR:FUNC\r\n", 10);
-            #endif
+            // #if MODBUS_DEBUG
+            //     UART_SendBytes((uint8_t*)"ERR:FUNC\r\n", 10);
+            // #endif
                 Modbus_Send_Error(func, 0x01); // 不支持的功能码
                 break;
         }
@@ -221,9 +221,9 @@ void Modbus_Read_Holding_Register(uint8_t *frame, uint8_t len)
 
     if (len < 8)
     {
-    #if MODBUS_DEBUG
-        UART_SendBytes((uint8_t*)"ERR:PARAM\r\n", 11);
-    #endif
+    // #if MODBUS_DEBUG
+    //     UART_SendBytes((uint8_t*)"ERR:PARAM\r\n", 11);
+    // #endif
         Modbus_Send_Error(0x03, 0x02);
         return;
     }
@@ -234,9 +234,9 @@ void Modbus_Read_Holding_Register(uint8_t *frame, uint8_t len)
     // 检查地址范围：支持0-9和0x07D0-0x07D1
     if (num == 0 || num > 64)
     {
-    #if MODBUS_DEBUG
-        UART_SendBytes((uint8_t*)"ERR:PARAM\r\n", 11);
-    #endif
+    // #if MODBUS_DEBUG
+    //     UART_SendBytes((uint8_t*)"ERR:PARAM\r\n", 11);
+    // #endif
         Modbus_Send_Error(0x03, 0x02);
         return;
     }
@@ -275,9 +275,9 @@ void Modbus_Read_Holding_Register(uint8_t *frame, uint8_t len)
         else
         {
             // 非法地址
-        #if MODBUS_DEBUG
-            UART_SendBytes((uint8_t*)"ERR:ADDR\r\n", 10);
-        #endif
+        // #if MODBUS_DEBUG
+        //     UART_SendBytes((uint8_t*)"ERR:ADDR\r\n", 10);
+        // #endif
             Modbus_Send_Error(0x03, 0x02);
             return;
         }
@@ -339,9 +339,9 @@ void Modbus_Write_Single_Register(uint8_t *frame, uint8_t len)
 {
     if (len < 8)
     {
-    #if MODBUS_DEBUG
-        UART_SendBytes((uint8_t*)"ERR:PARAM\r\n", 11);
-    #endif
+    // #if MODBUS_DEBUG
+    //     UART_SendBytes((uint8_t*)"ERR:PARAM\r\n", 11);
+    // #endif
         Modbus_Send_Error(0x06, 0x02);
         return;
     }
